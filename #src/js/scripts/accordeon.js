bbc4d461d168f79a8b25a4ff855037be14cf39f2
colocolov <colocolov.stas@gmail.com>
2022-04-29 12:56:36 +0300

    add all works + js

--- #src/js/scripts/accordeon.js
+++ #src/js/scripts/accordeon.js
@@ -1,15 +1,21 @@
 // accordeon
-document.addEventListener("DOMContentLoaded", () => {
-  const accordeons = document.querySelectorAll(".accordeon__item");
+const servicesBtn = document.querySelector(".services__btn");
+const servicesAll = document.querySelector(".services__all");
+const works = document.querySelector(".works");
+// const menuLink = document.querySelector(".menu__item");
+if (servicesBtn) {
+  servicesBtn.addEventListener("click", function (e) {
+    // document.body.classList.toggle("_lock");
+    servicesBtn.classList.toggle("active");
+    servicesAll.classList.toggle("active");
+    works.classList.toggle("active");
 
-  accordeons.forEach((el) => {
-    el.addEventListener("click", (e) => {
-      const self = e.currentTarget;
-      const title = self.querySelector(".accordeon__name");
-      const content = self.querySelector(".accordeon__decription");
-
-      self.classList.toggle("active");
-    });
+    // смена текста на кнопке "Все услуги" при клике
+    if (servicesBtn.classList.contains("active")) {
+      // console.log("yes");
+      servicesBtn.textContent = "Закрыть";
+    } else {
+      servicesBtn.textContent = "Все услуги";
+    }
   });
-});
-// ----- END
+}
